5c5cacc708d6d6797a9e91a1d2c69ab279f6ead1
thomasprice <thomas.ed.price@gmail.com>
2022-11-09 14:44:17 +0900

    added restaurant form

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem "bootsnap", require: false
 
 # custom gems
 gem "simple_form"
+gem "faker"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params)
+    @restaurant.save
+    redirect_to restaurants_path, status: :see_other
+  end
+
+  private
+
+  def strong_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+end
